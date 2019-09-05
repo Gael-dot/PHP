314e7d3c6dcd7fa16e97975e3b7bfd6189c60a91
Gael BOURGES <11802795@crit-xubuntu>
2019-09-05 17:27:08 +0200

    5 septembre 2019

---
+++ code.php
@@ -0,0 +1,7 @@
+<?php include ("debut_code_html.php") ?>
+
+<h1> <?php echo "$title"; ?> </h1>
+
+<?php include ("fin_code_html.php") ?>
+
+<p> Paragraphe en php </p>
---
+++ debut_code_html.php
@@ -0,0 +1,7 @@
+<!doctype html>
+<html>
+  <head>
+      <title> <?php echo 'Premiers pas en PHP'; ?> </title>
+      <meta charset="utf-8"/>
+  </head>
+  <body>
