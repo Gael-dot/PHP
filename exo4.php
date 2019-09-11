a7df0f84e2308506796962b455fb82f87666e936
Gael BOURGES <11802795@crit-xubuntu>
2019-09-11 17:30:13 +0200

    2eme seance

---
+++ exo4.php
@@ -0,0 +1,23 @@
+<?php
+
+$personnes = [
+  'mdupond' => ['Prénom' => 'Martin', 'Nom' => 'Dupond', 'Age' => 25, 'Ville' => 'Paris'       ],
+  'jm'      => ['Prénom' => 'Jean'  , 'Nom' => 'Martin', 'Age' => 20, 'Ville' => 'Villetaneuse'],
+  'toto'    => ['Prénom' => 'Tom'   , 'Nom' => 'Tonge' , 'Age' => 18, 'Ville' => 'Epinay'      ],
+  'arn'     => ['Prénom' => 'Arnaud', 'Nom' => 'Dupond', 'Age' => 33, 'Ville' => 'Paris'       ],
+  'email'   => ['Prénom' => 'Emilie', 'Nom' => 'Ailta' , 'Age' => 46, 'Ville' => 'Villetaneuse'],
+  'dask'    => ['Prénom' => 'Damien', 'Nom' => 'Askier', 'Age' => 7 , 'Ville' => 'Villetaneuse']
+];
+
+echo "<table>";
+  foreach ($personnes as $c => $v) {
+    echo "<tr>";
+    #echo "<th> $c2 </th> ";
+    foreach ($v as $c2 => $v2) {
+      echo "<td> $v2 </td> ";
+    }
+    echo "</tr>";
+  }
+echo "</table";
+
+?>
